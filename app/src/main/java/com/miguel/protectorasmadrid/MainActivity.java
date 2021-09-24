64d7926dbc21dd0c4c648d59d89c316c14c7c052
MiguelFdez <miguelfdez400@gmail.com>
2021-09-24 18:17:54 +0200

    Pequeños avances de prueba

--- app/src/main/java/com/miguel/protectorasmadrid/MainActivity.java
+++ app/src/main/java/com/miguel/protectorasmadrid/MainActivity.java
@@ -8,6 +8,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //No es necesario
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_ProtectorasMadrid);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
